40a786218b539d6647ec89426ab128a629d6cd09
RyanAVelasco <45979367+RyanAVelasco@users.noreply.github.com>
2021-07-20 23:15:40 -0500

    feature: look up contacts
    Added ability to:
    look up specific contact,
    display all contacts,

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ if not address_book in os.listdir():
     file.close()
 else: print(address_book)
 
+def open_address_book():
+    file = open(address_book, 'r')
+    contacts = file.readlines()
+    ## assures the file is closed once done
+    file.close()
+    return contacts
+
 ## creates contact information
 def create_contact(name, address, email, phone_number):
-    file = open(address_book, 'r')
-
-    ## save contact information to address book
-    contacts = file.readlines()
-    print(contacts)
-    if name + ';' + address + ';' + email + ';' + phone_number + '\n' in contacts : print('already exists')
+    if name + ';' + address + ';' + email + ';' + phone_number + '\n' in open_address_book() : print('already exists')
     else: 
-        file.close()
         file = open(address_book, 'a')
         file.write(name + ';' + address + ';' + email + ';' + phone_number + '\n')
 
@@ -28,17 +29,24 @@ def create_contact(name, address, email, phone_number):
     file.close()
 
 def check_exists(info):
-
     ## checks for contact name in address book
-    file = open(address_book, 'r')
-    lines = file.readlines()
-    for line in lines:
-        if info in line:
-            print('\nSorry, contact already exists!' + '\n>> ' + line.replace(';', ' -- '))
+    for contact in open_address_book():
+        if info in contact.split(';'):
             return True
         else : continue
 
-    ## assures the file is closed once done
+def search_contacts(full_list, contact_name):
+    file = open(address_book, 'r')
+    contacts = file.readlines()
+
+    if full_list == True:
+        for contact in contacts:
+            print('>>', contact.replace(';', ', ').replace('\n', ''))
+    elif full_list == False:
+        for contact in contacts:
+            if contact_name in contact.split(';'):
+                print('>>', contact.replace(';', ', ').replace('\n', ''))
+            continue
     file.close()
     
 while True:
@@ -54,32 +62,37 @@ while True:
     if options == '1' or options == 'add contact':
         while True:
             ## if contact name is already in address book then return to main menu
-            name = input('\nFirst Name: ').title().strip()
-            if check_exists(name) == True : break
+            first_name = input('First Name: ').title().lstrip()  
+            last_name = input('Last name: ').title().rstrip()  
+            name = first_name + ' ' + last_name
 
-            address = input('\nHome Address: ').title().strip()
-            email = input('\nEmail: ').strip()
-            phone_number = input('\nPhone number: ').strip()
+            if check_exists(name) == True : 
+                print('>> Contact', name, 'exists')
+                break
+
+            address = input('Home Address: ').title().strip()
+            email = input('Email: ').strip()
+            phone_number = input('Phone number: ').strip()
 
             create_contact(name, address, email, phone_number)
+            print('\n[ADDED]', '\nName:', name, '\nAddress:', address, '\nEmail:', email, '\nPhone Number:', phone_number)
             break
-        
-        # elif options == '2' or options == 'add contact':
-        #     pass
-        # elif options == '3' or options == 'add contact':
-        #     pass
-        # elif options == '4' or options == 'add contact':
-        #     pass
+    
+    elif options == '2' or options == 'Delete contact':
+        pass
 
-    if options == '5' or options == 'exit' or options == 'exit program':
+    elif options == '3' or options == 'lookup contact':
+        contact_name = input('Please enter name: ').title().strip()
+        search_contacts(False, contact_name)
+
+    elif options == '4' or options == 'lookup all contacts':
+        search_contacts(True, None)
+
+    elif options == '5' or options == 'exit' or options == 'exit program':
         exit = input('\nThanks for using address book\nShould I show you your address book before exitting? (Y/N): ').strip()
         if exit.lower() == 'yes' or exit.lower() =='y':
             print('\nHere you go')
-            time.sleep(2)
-            file = open(address_book, 'r')
-            lines = file.readlines()
-            for line in lines:
-                print(line.replace(';', ' -- '))
+            search_contacts(True, None)
             quit()
         else : quit()
         
